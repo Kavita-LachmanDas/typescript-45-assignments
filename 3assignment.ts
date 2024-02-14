e8d9526f29e0bc9cf179b1a97f5dadd3a5631d01
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:49:56 -0800

    Add files via upload

---
+++ 3assignment.ts
@@ -0,0 +1,16 @@
+//Name Cases: Store a person’s name in a variable,
+// and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName:string = "Kavita " 
+// name in uppercase
+let uppercasename:string = personName.toUpperCase()
+console.log("Uppercase: " + uppercasename)
+
+//name in lower case
+let lowercasename:string = personName.toLowerCase()
+console.log("Lowercase: "+lowercasename)
+
+//name in title case
+let titlecasename:string = personName.toString()
+let title = ""
+console.log("Titlecase: "+ titlecasename)
