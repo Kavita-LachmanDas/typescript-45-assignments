144ff2fb6e4c559e97fb0aec2bba428f3ed3e885
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:57:48 -0800

    Add files via upload

---
+++ 21assignment.ts
@@ -0,0 +1,17 @@
+//They think of something you could store in a TypeScript Object.
+// Write a program that creates Objects containing these items
+
+type details={
+  Name:string
+  FathersName:string
+  Class:number
+  RollNo:number
+  
+}
+let object:details={
+    Name:"Kavita",
+     FathersName:"LachamnDas",
+     Class:12,
+     RollNo:217200
+}
+console.log(object)
