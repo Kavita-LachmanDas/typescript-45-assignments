ced937445c1c35e29a5007201d5640b282ce1a21
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:52:43 -0800

    Add files via upload

---
+++ 10assignment.ts
@@ -0,0 +1,41 @@
+//Adding Comments: Choose two of the programs you’ve written,
+// and add at least one comment to each. If you don’t have anything specific 
+//to write because your programs are too simple at this point, just add your 
+//name and the current date at the top of each program file. Then write one 
+//sentence describing what the program does.
+
+//1. add comment 
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+//create a message that reveals your favorite number. Print that message.
+
+//Name: KAVITA
+//  Date: 
+var FavNum:number = 7
+console.log(FavNum + " because it shows the quantity of my family members")
+// 7: number is my most favorite numbers because it shows the quantity of my family members and
+// it my lucky number. THIS IS THE REASON.
+//and u say what program does
+//first we make a variable for storing the data,
+// variable is act like a container it stores every type of data like
+// string, number,boolean(these are the data type actually these are called primitive data type)
+//then anything we have to print then we write console.log() then we get result otherwise we don't 
+// write console.log() then we cant get results 
+
+
+//2. // Stripping Names: Store a person’s name,
+// and include some whitespace characters at the beginning 
+//and end of the name. Make sure you use each character combination, "\t" and "\n", 
+//at least once. Print the name once, so the whitespace around the name is displayed. 
+//Then print the name after striping the white spaces.
+
+// Name: kavita
+//Date: 
+var name5 = "\t kavita /n"
+console.log("Nmae With White Space: " + name5)
+var name5 = "kavita"
+console.log("Name Without White space: " + name5)
+//when we write white space character we must have to write "\t" and '/n"
+//otherwise we do not write these character then we cant get white space
+
+
+//complete//
