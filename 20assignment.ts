71395526e11a4aadc024b1d9411a211558714488
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:57:26 -0800

    Add files via upload

---
+++ 20assignment.ts
@@ -0,0 +1,51 @@
+//Think of something you could store in a array.
+// For example, you could make a list of mountains, rivers, countries, 
+//cities, languages, or anything else you’d like. Write a program that 
+//creates a list containing these items.
+
+let languages:string[]=["Sindhi","Urdu","English","French","Japanese","Punjabi","Hindhi"]
+let mountains:string[]=
+["Mount Everest","Dhaulagiri", "Nepal","K2","Karakoram","Hindu Kush","Salt Range","Himaliya"]
+let cities:string[]=["Mirpur Bathoro","Sujawal","Hyderabad","Karachi","Thatta","Nawabshah"]
+let countries:string[]=["Pakistan","India","Afghanistan","Switzerland","Japan"]
+let rivers:string[]=["Indus","Jhelum","Chenab","Sutlej","Ravi"," Kabul"]
+console.log("Languages: " + languages[0])
+console.log(languages[1])
+console.log(languages[2])
+console.log(languages[3])
+console.log(languages[4])
+console.log(languages[5])
+console.log(languages[6])
+console.log(`\n`)
+//mountains
+console.log("Mountains: " + mountains[0])
+console.log(mountains[1])
+console.log(mountains[2])
+console.log(mountains[3])
+console.log(mountains[4])
+console.log(mountains[5])
+console.log(mountains[6])
+console.log(mountains[7])
+console.log(`\n`)
+//cities
+console.log("Cities: " + cities[0])
+console.log(cities[1])
+console.log(cities[2])
+console.log(cities[3])
+console.log(cities[4])
+console.log(cities[5])
+console.log(`\n`)
+//countries
+console.log("Countries: " + countries[0])
+console.log(countries[1])
+console.log(countries[2])
+console.log(countries[3])
+console.log(countries[4])
+console.log(`\n`)
+//rivers
+console.log("Rivers: " + rivers[0])
+console.log(rivers[1])
+console.log(rivers[2])
+console.log(rivers[3])
+console.log(rivers[4])
+console.log(rivers[5])
