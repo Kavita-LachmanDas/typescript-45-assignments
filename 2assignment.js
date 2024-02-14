9d8c21ba8fdbeda5fd77f1e29c92f8124b95fb85
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:49:13 -0800

    Add files via upload

---
+++ 2assignment.js
@@ -0,0 +1,6 @@
+"use strict";
+//Personal Message: Store a person’s name in a variable, and print a message to that person.\
+// Your message should be simple, such as,
+// “Hello Eric, would you like to learn some Python today?”
+var name1 = "Kavita lachamndas";
+console.log("hello " + name1 + ",  would you like to learn some python today?");
