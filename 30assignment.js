f7c701c646043c0d0d0eceb739280c695798c829
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 03:01:54 -0800

    Add files via upload

---
+++ 30assignment.js
@@ -0,0 +1,23 @@
+//Hello Admin: Make a array of five or more usernames, including the name 'admin'.
+// Imagine you are writing code that will print a greeting to each user after they log in to a website.
+// Loop through the array, and print a greeting to each user:
+//• If the username is 'admin', print a special greeting, such as Hello admin,
+// would you like to see a status report?
+
+//• Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+let UserName = ["Kavita","Bhawna","Preeti","Kirshna","Lachman","Chandra"]
+for(let i = 0; i <= 0; i++) {
+    console.log(UserName [0]  + " Hello admin, would you like to see the a status report")
+      console.log(UserName[1] + " Hello, thank you for logging in again.")
+     console.log(UserName[2] + " Hello, thank you for logging in again.")
+     console.log(UserName[3] + " Hello, thank you for logging in again.")
+     console.log(UserName[4] + " Hello, thank you for logging in again.")
+     console.log(UserName[5] + " Hello, thank you for logging in again.")
+
+}
+
+
+
+
+
