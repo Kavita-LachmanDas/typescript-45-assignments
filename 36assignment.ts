a6c47af7b9b3fb4a7a69cb802c1bd2f461eee793
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 03:04:50 -0800

    Add files via upload

---
+++ 36assignment.ts
@@ -0,0 +1,11 @@
+//T-Shirt: Write a function called make_shirt() that accepts a size and the
+// text of a message that should be printed on the shirt. 
+//he function should print a sentence summarizing the size 
+//of the shirt and the message printed on it. Call the function.
+
+function makeShirt(size: string, message: string): void {
+    console.log(`You've ordered a ${size} sized shirt with the message: "${message}".`);
+}
+
+// Calling the function
+makeShirt("Large", "Hello, World!");
