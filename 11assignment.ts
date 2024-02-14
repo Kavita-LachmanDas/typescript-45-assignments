ab69e22cb850e4120fc0ea9001ff1323e177b7e1
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:53:06 -0800

    Add files via upload

---
+++ 11assignment.ts
@@ -0,0 +1,11 @@
+//Names: Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+
+var names:string[] = ["Preeti", "Bhawna", "Kirshna", "Chandra", "Kavita", "Lachmandas","Archna"]
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
+console.log(names[5])
+console.log(names[6])
