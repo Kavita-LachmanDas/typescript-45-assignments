854a147f474b54713994b748cef4e5e902f8567a
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:53:25 -0800

    Add files via upload

---
+++ 12assignment.ts
@@ -0,0 +1,14 @@
+//Greetings: Start with the array you used in Exercise 11, \
+//but instead of just printing each person’s name, print a message to them.
+// The text of each message should be the same,
+// but each message should be personalized with the person’s name
+
+
+var names:string[] = ["Preeti", "Bhawna", "Kirshna", "Chandra", "Kavita", "Lachmandas","Archna"]
+console.log(names[0] + ": i hope you are doing well.")
+console.log(names[1] + ": i hope you are doing well.")
+console.log(names[2] + ": i hope you are doing well.")
+console.log(names[3] + ": i hope you are doing well.")
+console.log(names[4] + ": i hope you are doing well.")
+console.log(names[5] + ": i hope you are doing well.")
+console.log(names[6] + ": i hope you are doing well.")
