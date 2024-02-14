78b3157a9cab23564a1e6359d0e99811c7e6a6c5
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:58:13 -0800

    Add files via upload

---
+++ 22assignment.ts
@@ -0,0 +1,13 @@
+//Intentional Error: If you haven’t received an array index error
+// in one of your programs yet, try to make one happen. Change an index in one of your
+// programs to produce an index error. 
+//Make sure you correct the error before closing the program.
+
+
+//index error
+let array:boolean[]=[true,false]
+console.log(array)
+
+//after correction
+let array1:boolean[]=[true,false]
+console.log(array1)
