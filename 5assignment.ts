8a77e61f1b81cdb19eb8cd4da28522509d64e6c6
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:50:47 -0800

    Add files via upload

---
+++ 5assignment.ts
@@ -0,0 +1,7 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name 
+//in a variable called famous_person.
+// Then compose your message and store it in a new variable called message. Print your message.
+
+let FamousPerson:string = "Quaid e azam:"
+let msg:string =' "Think 100 times before you take a decision, But once that decision is taken, stand by it as one man."'
+console.log(FamousPerson + msg)
