3b9c6026a7d7e46c4a1e1efa8ba99e5cd20fdd47
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:54:36 -0800

    Add files via upload

---
+++ 14assignment.ts
@@ -0,0 +1,9 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, 
+//who would you invite? Make a list that includes at least
+// three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+
+var invite:string[]= ["Saima", "Shahbaz", "Naeem",]
+console.log(invite[0] + ": i would like to invite you to join me at dinner")
+console.log(invite[1] + ": i would like to invite you to join me at dinner")
+console.log(invite[2] + ": i would like to invite you to join me at dinner")
