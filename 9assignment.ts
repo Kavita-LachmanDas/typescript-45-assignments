08a637234d9c8ee7ac399d27a051ab5b484da971
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:52:23 -0800

    Add files via upload

---
+++ 9assignment.ts
@@ -0,0 +1,5 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+//create a message that reveals your favorite number. Print that message.
+
+var FavNum:number = 7
+console.log(FavNum + " because it shows the quantity of my family members")
