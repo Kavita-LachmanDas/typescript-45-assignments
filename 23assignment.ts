d4f10a39fcfa378f2c92c320e9216c2f39ae1368
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:58:34 -0800

    Add files via upload

---
+++ 23assignment.ts
@@ -0,0 +1,60 @@
+//Conditional Tests: Write a series of conditional tests. 
+//Print a statement describing each test and your prediction
+// for the results of each test. Your code should look something like this:
+
+//let car = 'subaru';
+
+//console.log("Is car == 'subaru'? I predict True.")
+
+//console.log(car == 'subaru')
+
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// 5 tests evaluate to True
+let car = "Audi"
+console.log("Is car =='Audi'? I Predict True.")
+console.log(car==="Audi")
+
+let Name = "kavita"
+console.log("Is name =='kavita' ? I Predict True.")
+console.log(Name=="kavita")
+
+let num = 7
+console.log("Is num ==7 ? I Predict True.")
+console.log(num==7)
+
+let a = 7
+let b = 7
+console.log("Is a == b ? I Predict True.")
+console.log(a==b)
+
+let k = 53
+let h = 45
+console.log("Is k >= h ? I Predict True.")
+console.log(k >= h)
+
+console.log(`\n`)
+// 5 test of true is completed//
+
+//another 5 tests evaluate to False.
+let j = 53
+let l = 45
+console.log("Is j == l ? I Predict False.")
+console.log(j==l)
+
+let Name2 = "kavita"
+console.log("Is name2 =='luhana' ? I Predict False.")
+console.log(Name2=="luhana")
+
+let car2 = "Audi"
+console.log("Is car2 ==scooter? I Predict False.")
+console.log(car=='scooter')
+
+let num1 = 0
+console.log("Is num ==0 ? I Predict False.")
+console.log(num==0)
+
+let computer = "laptop"
+console.log("Is computer == 'hpLaptop'? I Predict False")
+console.log(computer=="hpLaptop")
