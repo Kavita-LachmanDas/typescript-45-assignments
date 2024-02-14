ccbbb136775657979002846e817eec38a15dbaac
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:52:02 -0800

    Add files via upload

---
+++ 8assignment.ts
@@ -0,0 +1,10 @@
+//You should create four lines that look like this:
+
+//console.log(5 + 3)
+
+//Your output should simply be four lines with the number 8 appearing once on each line.
+
+console.log(5 + 3)
+console.log(4 + 4)
+console.log(4 * 2)
+console.log(64 / 8)
