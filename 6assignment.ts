6bba198d6f5c6321bfbb1eb1d073e22c5c7c79b1
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:51:06 -0800

    Add files via upload

---
+++ 6assignment.ts
@@ -0,0 +1,10 @@
+// Stripping Names: Store a person’s name,
+// and include some whitespace characters at the beginning 
+//and end of the name. Make sure you use each character combination, "\t" and "\n", 
+//at least once. Print the name once, so the whitespace around the name is displayed. 
+//Then print the name after striping the white spaces.
+
+var name5 = "\t kavita /n"
+console.log("Nmae With White Space: " + name5)
+var name5 = "kavita"
+console.log("Name Without White space: " + name5)
