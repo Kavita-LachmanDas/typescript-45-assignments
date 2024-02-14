d0396615bdd78095b1a8c6fa073fd071d9940c75
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:57:03 -0800

    Add files via upload

---
+++ 19assignment.ts
@@ -0,0 +1,35 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+// print a message indicating the number of people you are inviting to dinner.
+
+
+var invite= ["Saima", "Shahbaz", "Naeem",]
+console.log(invite)
+invite.pop()
+console.log(invite)
+invite.push("Rubina" ,"Aslam","kirshna","Dolly","minahil","mohina","kavya")
+console.log(invite)
+invite.unshift("kavita")
+console.log(invite)
+invite.splice(2, + "Irfan" , "hassan")
+console.log(invite)
+console.log(invite[0] + ": i would like to invite you to join me at dinner")
+console.log(invite[1] + ": i would like to invite you to join me at dinner")
+console.log(invite[2] + ": i would like to invite you to join me at dinner")
+console.log(invite[3] + ": i would like to invite you to join me at dinner")
+
+//print a new set of invitatation
+//i found a bigger dinner table//
+console.log(invite[0] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[1] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[2] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[3] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[4] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[5] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[6] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[7] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[8] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[9] + "  i would like to invite you at dinner and i found a bigger dinner table")
+console.log(invite[10] + "  i would like to invite you at dinner and i found a bigger dinner table")
+//indicating the number of people which i have invited
+let invitepeople= invite.push()
+console.log(invitepeople)
