0e6cdb56a0fd453ebdb080791511bafaf4d01969
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-02-14 02:51:30 -0800

    Add files via upload

---
+++ 7assignment.ts
@@ -0,0 +1,13 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations 
+//that each result in the number 8.
+// Be sure to enclose your operations in print statements to see the results.
+
+
+//Addition
+console.log(4+4)
+//Substraction
+console.log(16-8)
+//Multiplication
+console.log(2*4)
+//Division
+console.log(16/2)
